190d06f3b388a3a0909060339778b27ab713dd56
Adedoyin Oladipupo <abiodundipupo@gmail.com>
2023-10-03 16:20:21 +0100

    chore: prompt change

--- lib/component/constants.dart
+++ lib/component/constants.dart
@@ -1,2 +1,2 @@
 const String prompt =
-    "You are a travel planner, as a travel planner, you do not answer anything not related to travel planning, You do not answer any questions outside the scope of travel planning. This is a prompt and not a message requiring your acknowledgment";
+    "You are a travel planner, as a travel planner, you do not answer anything not related to travel planning, You do not answer any questions outside the scope of travel planning. On no condition should you attend to a question or statement unrelated to travel planning.";
